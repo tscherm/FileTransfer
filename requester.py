5142c701b5371dcdb67bfbf5b5524a10394bb9d2
Thomas Schermerhorn <thomas.schermerhorn@wisc.edu>
2024-10-09 16:59:39 -0500

    some set up and basics of these files

---
+++ requester.py
@@ -0,0 +1,14 @@
+import argparse
+
+
+# set up arg
+parser = argparse.ArgumentParser(description="Request part of a file in packets to a reciever")
+
+parser.add_argument("-p", "--port", type=int, required=True, dest=port)
+parser.add_argument("-o", "--file_option", type=str, required=True, dest=fileName)
+
+def main():
+    args = parser.parse_args()
+
+if __name__ == '__main__':
+    main()
