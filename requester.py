ab38baa6af17e4549c8fdccbfef2fa9e56b0d99f
Tom Schermerhorn <tomjschermerhorn@gmail.com>
2024-10-09 17:09:30 -0500

    fixed some warnings

--- requester.py
+++ requester.py
@@ -1,11 +1,11 @@
 import argparse
-
+import socket
 
 # set up arg
 parser = argparse.ArgumentParser(description="Request part of a file in packets to a reciever")
 
-parser.add_argument("-p", "--port", type=int, required=True, dest=port)
-parser.add_argument("-o", "--file_option", type=str, required=True, dest=fileName)
+parser.add_argument("-p", "--port", type=int, required=True, dest="port")
+parser.add_argument("-o", "--file_option", type=str, required=True, dest="fileName")
 
 def main():
     args = parser.parse_args()
--- sender.py
+++ sender.py
@@ -1,24 +1,25 @@
 import argparse
-
+import socket
 
 # set up arg
 parser = argparse.ArgumentParser(description="Send part of a file in packets to a reciever")
 
-parser.add_argument("-p", "--sender_port", type=int, required=True, dest=sPort)
-parser.add_argument("-g", "--requester_port", type=int, required=True, dest=rPort)
-parser.add_argument("-r", "--rate", type=int, required=True, dest=rate)
-parser.add_argument("-q", "--seq_no", type=int, required=True, dest=seqNo)
-parser.add_argument("-l", "--length", type=int, required=True, dest=length)
+parser.add_argument("-p", "--sender_port", type=int, required=True, dest="sPort")
+parser.add_argument("-g", "--requester_port", type=int, required=True, dest="rPort")
+parser.add_argument("-r", "--rate", type=int, required=True, dest="rate")
+parser.add_argument("-q", "--seq_no", type=int, required=True, dest="seqNo")
+parser.add_argument("-l", "--length", type=int, required=True, dest="length")
 
 args = parser.parse_args()
 
 def printPacket(ptype, time, destAddr, seqNo, length, payload):
-    print("{ptype} Packet")
-    print("send time:\t{(time.strftime("%y-%m-%d %H:%M:%S.%f"))[:-3]}")
-    print("requester addr:\t{destAddr}:{}")
-    print("Sequence num:\t{seqNo}")
-    print("length:\t{len}")
-    print("payload:\t{payload}\n")
+    print(f"{ptype} Packet")
+    timeStr = (time.strftime("%y-%m-%d %H:%M:%S.%f"))[:-3]
+    print(f"send time:\t{timeStr}")
+    print(f"requester addr:\t{destAddr}:{args.rPort}")
+    print(f"Sequence num:\t{seqNo}")
+    print(f"length:\t{length}")
+    print(f"payload:\t{payload}\n")
 
 def main():
     print("STARTED")
